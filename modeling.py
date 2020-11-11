fff63907009ea65045e7a13cff4353a59e6fd54b
Navish Agarwal <navish.16@gmail.com>
2020-11-11 13:52:58 +0530

    topic modelling notebooks, helper functions

--- modeling.py
+++ modeling.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.decomposition import TruncatedSVD
-from sklearn.decomposition import NMF
+from sklearn.decomposition import TruncatedSVD, LatentDirichletAllocation, NMF
 from sklearn.metrics.pairwise import cosine_similarity
+
+def lda_topic_modeling(word_matrix, vocab, n = 5):
+
+    lda = LatentDirichletAllocation(n_components=n, random_state=0, max_iter = 100, n_jobs = -1, verbose = 1)
+    lda.fit(word_matrix)
+    topic_matrix = pd.DataFrame(lda.transform(word_matrix)).add_prefix("topic_")
+    word_matrix = pd.DataFrame(lda.components_, \
+        columns = vocab).T.add_prefix('topic_')
+
+    return lda, lda.bound_, topic_matrix, word_matrix
 
 def nmf_topic_modeling (word_matrix, vocab, n = 5):
 
-    nmf = NMF(n_components = n)
+    nmf = NMF(n_components = n, max_iter = 1000)
     nmf.fit(word_matrix)
 
     topic_matrix = pd.DataFrame(nmf.transform(word_matrix)).add_prefix("topic_")
@@ -18,18 +27,30 @@ def nmf_topic_modeling (word_matrix, vocab, n = 5):
 
 def top_reviews(topic_matrix_df, topic, n_reviews):
     return (topic_matrix_df
-            .sort_values(by=topic, ascending=False)
+            .sort_values(by=f'topic_{topic}', ascending=False)
             .head(n_reviews)['raw_review']
             .values)
 
 def top_words(word_topic_matrix_df, topic, n_words):
     return (word_topic_matrix_df
-            .sort_values(by=topic, ascending=False)
-            .head(n_words))[topic]
+            .sort_values(by=f'topic_{topic}', ascending=False)
+            .head(n_words))[f'topic_{topic}']
 
+def top_words_for_all_topics(word_matrix, n_topics, n_words):
+    for i in range(n_topics):
+        topic = "topic_" + str(i)
+        print(f'Topic {i}')
+        words = top_words(word_matrix, topic, 10).index
+        for word in words:
+            print(word, end = ', ')
+        print('\n')
+    
+    return None
+
+    
 def lsa_topic_modeling(word_matrix, vocab, n = 5):
 
-    lsa = TruncatedSVD(10)
+    lsa = TruncatedSVD(n)
     lsa.fit(word_matrix)
     topic_matrix = pd.DataFrame(lsa.transform(word_matrix)).add_prefix('topic_')
     # lsa.explained_variance_ratio_
@@ -37,3 +58,5 @@ def lsa_topic_modeling(word_matrix, vocab, n = 5):
     word_matrix = pd.DataFrame(lsa.components_, columns = vocab).T.add_prefix('topic_')
 
     return lsa, lsa.explained_variance_, topic_matrix, word_matrix
+
+# def lda_  
--- nlp_cleaning.py
+++ nlp_cleaning.py
@@ -20,22 +20,45 @@ import nlp_cleaning
 import string
 import spacy
 
-sp_nlp = spacy.load('en', disable=[ 'parser', 'ner'])
+sp_nlp = spacy.load('en', disable=['parser'])
 
-def remove_stopwords(text):
+def remove_stopwords(text, remove_words_list = stopwords.words('english')):
     tokens = word_tokenize(text)
     tokens = [token.strip() for token in tokens]
-    tokens = [token for token in tokens if token not in stopwords_list]
+    tokens = [token for token in tokens if token not in remove_words_list]
     return ' '.join(tokens)
 
+def ner_list(text):
+    spacy_text = sp_nlp(text)
+    return spacy_text.ents
+
+def spacy_pos(text):
+    spacy_text = sp_nlp(text)
+    pos = [token.pos_ for token in spacy_text]
+    return pos
+
+def spacy_pos_entities(text):
+    spacy_text = sp_nlp(text)
+    return [[token.text, token.lemma_, token.pos_, token.ent_type_] for token in spacy_text]
 
 def spacy_lemmatization(text):
     spacy_text = sp_nlp(text)
-    tokens_lemma = [token.lemma_ for token in spacy_text ]
+    tokens_lemma = [token.lemma_ for token in spacy_text]
     text_lemma = ' '.join(tokens_lemma)
     tokens_lemma_remove_pron = re.sub(r'-PRON-', '', text_lemma)
     
     return tokens_lemma_remove_pron
+
+def spacy_pos_filtering(text, pos = [], ent_label = []):
+    spacy_text = sp_nlp(text)
+    
+    # [print(token, token.text, token.pos_, token.ent_type_) for token in spacy_text]
+    tokens = [token.text for token in spacy_text if ((token.pos_ in pos) and (token.ent_type_ not in ent_label))]
+    
+    filtered_text = ' '.join(tokens)
+    
+    return filtered_text
+
 
 def cleaning(text):
     """
@@ -66,18 +89,6 @@ def cleaning(text):
 def remove_stop_words(text):
     tokens = word_tokenize(text)
     tokens = [t for t in tokens if t not in stopwords.words('english')]
-# def cleaning(text, regexp = r'[^A-Za-z\s]', sub = ''):
-#     """
-#     Clean the provided text using the regexp
 
-#     Args:
-#         text (str): Text string to be cleaned
-#         regexp (regexp, optional):  Defaults to r'[^A-Za-z\s]'.
-
-#     Returns:
-#         text: cleaned text based on the regex provided
-#     """
-
-#     return re.sub(regexp, '', text)
 
                 
